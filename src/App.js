fa2d1a5bbb955329d576d84461e5f6d5952ad3ae
rinlama <rinjinlama83@gmail.com>
2021-07-17 18:47:11 -0400

    add select dropdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Multiselect from "multiselect-react-dropdown";
 
 function App() {
+  const [food, setFood] = useState(["Burger", "Pizza", "Sandwich"]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Multiselect
+        isObject={false}
+        onRemove={(event) => {
+          console.log(event);
+        }}
+        onSelect={(event) => {
+          console.log(event);
+        }}
+        options={food}
+        selectedValues={["Burger"]}
+        showCheckbox
+      />
     </div>
   );
 }
