8fac14888b14ee41389f30ba1d0598ba5365fd5c
rinlama <rinjinlama83@gmail.com>
2021-09-27 21:58:41 -0400

    add form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [loginState, setLoginState] = useState({
+    email: "",
+    password: "",
+    checkMeOut: false,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container my-5">
+      {JSON.stringify(loginState)}
+      <form>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={(event) => {
+              const value = event.target.value;
+              setLoginState({ ...loginState, email: value });
+            }}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            onChange={(event) => {
+              const value = event.target.value;
+              setLoginState({ ...loginState, password: value });
+            }}
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+            onChange={(event) => {
+              const value = event.target.checked;
+              setLoginState({ ...loginState, checkMeOut: value });
+            }}
+          />
+          <label className="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          disabled={!loginState.email || !loginState.password}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
