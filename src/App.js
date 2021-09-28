95b53f7ccb9b964a3f071592c78b4887ca4d8d25
rinlama <rinjinlama83@gmail.com>
2021-09-27 21:34:14 -0400

    semantic ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Progress } from "semantic-ui-react";
+import { useState } from "react";
 
 function App() {
+  const [percent, setPercent] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Progress percent={percent} indicating />
+      <Button
+        onClick={() => {
+          setPercent((p) => p + 10);
+        }}
+      >
+        Increment
+      </Button>
     </div>
   );
 }
