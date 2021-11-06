6c11ae6a30a31f5859fb1bfa45d4c2da32412c80
rinlama <rinjinlama83@gmail.com>
2021-11-06 13:09:10 -0400

    reduxtoolkits

--- src/App.js
+++ src/App.js
@@ -1,71 +1,32 @@
 import "./App.css";
-import { useState } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./store/features/counter/counterSlice";
 
 function App() {
-  const [loginState, setLoginState] = useState({
-    email: "",
-    password: "",
-    checkMeOut: false,
+  const count = useSelector((state) => {
+    return state.counter.value;
   });
-
+  const dispatch = useDispatch();
   return (
     <div className="container my-5">
-      {JSON.stringify(loginState)}
-      <form>
-        <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            onChange={(event) => {
-              const value = event.target.value;
-              setLoginState({ ...loginState, email: value });
-            }}
-          />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
         </div>
-        <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            onChange={(event) => {
-              const value = event.target.value;
-              setLoginState({ ...loginState, password: value });
-            }}
-          />
-        </div>
-        <div className="mb-3 form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-            onChange={(event) => {
-              const value = event.target.checked;
-              setLoginState({ ...loginState, checkMeOut: value });
-            }}
-          />
-          <label className="form-check-label" for="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button
-          type="submit"
-          className="btn btn-primary"
-          disabled={!loginState.email || !loginState.password}
-        >
-          Submit
-        </button>
-      </form>
+      </div>
     </div>
   );
 }
