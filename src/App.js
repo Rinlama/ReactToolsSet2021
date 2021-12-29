335169cb8ae256676524716a9dc0a0953cdc4f83
rinlama <rinjinlama83@gmail.com>
2021-12-29 18:44:03 -0500

    react final form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Form, Field } from "react-final-form";
 
 function App() {
+  const onSubmit = (data, formData) => {
+    // console.log(data);
+  };
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values.firstname) {
+      errors.firstname = "Firstname is required";
+    }
+    if (!values.lastname) {
+      errors.lastname = "Lastname  is required";
+    }
+
+    return errors;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <Form
+        onSubmit={onSubmit}
+        validate={validate}
+        initialValues={{
+          firstname: "",
+          lastname: "",
+          employee: true,
+          favoriteColor: "",
+        }}
+        render={({ handleSubmit, values }) => (
+          <form className="row g-3 needs-validation" onSubmit={handleSubmit}>
+            <Field name="firstname">
+              {({ input, meta }) => (
+                <div className="col-md-4">
+                  <label className="form-label">First name</label>
+                  <input
+                    type="text"
+                    className={
+                      meta.error && meta.touched
+                        ? "form-control is-invalid"
+                        : "form-control"
+                    }
+                    {...input}
+                  />
+                  {meta.error && meta.touched ? (
+                    <span className="invalid-feedback">{meta.error}</span>
+                  ) : (
+                    false
+                  )}
+                </div>
+              )}
+            </Field>
+            <Field name="lastname">
+              {({ input, meta }) => (
+                <div className="col-md-4">
+                  <label className="form-label">Last name</label>
+                  <input
+                    type="text"
+                    className={
+                      meta.error && meta.touched
+                        ? "form-control is-invalid"
+                        : "form-control"
+                    }
+                    {...input}
+                  />
+                  {meta.error && meta.touched ? (
+                    <span className="invalid-feedback">{meta.error}</span>
+                  ) : (
+                    false
+                  )}
+                </div>
+              )}
+            </Field>
+            <Field name="employee" type="checkbox">
+              {({ input, meta }) => (
+                <div className="col-md-4">
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      {...input}
+                      checked={values.employee}
+                    />
+                    <label className="form-check-label" for="flexCheckDefault">
+                      Employee
+                    </label>
+                  </div>
+                </div>
+              )}
+            </Field>
+            <div className="col-md-4">
+              <label className="form-check-label" for="flexCheckDefault">
+                Color
+              </label>
+              <Field
+                name="favoriteColor"
+                component="select"
+                className="form-select"
+              >
+                <option />
+                <option value="Red">❤️ Red</option>
+                <option value="Green">💚 Green</option>
+                <option value="Blue">💙 Blue</option>
+              </Field>
+            </div>
+
+            <div className="col-12">
+              <button className="btn btn-primary" type="submit">
+                Submit form
+              </button>
+            </div>
+            <pre>{JSON.stringify(values, 0, 2)}</pre>
+          </form>
+        )}
+      ></Form>
     </div>
   );
 }
